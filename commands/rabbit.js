e347ee5016998a93c7d227e0e19c90db9b2388a5
rafraf9 <31069991+rafraf9@users.noreply.github.com>
2020-11-22 14:02:01 +0200

    Update rabbit.js

--- commands/rabbit.js
+++ commands/rabbit.js
@@ -3,6 +3,8 @@ module.exports = {
     description: 'the lifes meaning',
     execute(message, args)
     {
-        message.channel.send('once upon a time there was a rabbit, the rabbits name was fluffy and he liked eating horses and carrots, one time he was looking for a little tasty horse but he didnt find!!! so he didnt know what to do so finally fluffy decided to be vegetarien and since then he ate just carrots.');
+        message.channel.send('once upon a time there was a rabbit, the rabbits name was fluffy and he liked eating horses and carrots,');
+        message.channel.send('one time he was looking for a little tasty horse but he didnt find!!!');
+        message.channel.send('so he didnt know what to do so finally fluffy decided to be vegetarien and since then he ate just carrots.');
     }
 }
