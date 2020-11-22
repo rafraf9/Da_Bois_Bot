2007e9aac86528bf2b51ac4f875e942b4defc1cc
rafraf9 <31069991+rafraf9@users.noreply.github.com>
2020-11-22 14:04:34 +0200

    Update rabbit.js

--- commands/rabbit.js
+++ commands/rabbit.js
@@ -1,6 +1,6 @@
 module.exports = {
-    name: 'MegaSuperBestRabbitStory',
-    description: 'the lifes meaning',
+    name: "rabbit_story",
+    description: "the lifes meaning",
     execute(message, args)
     {
         message.channel.send('once upon a time there was a rabbit, the rabbits name was fluffy and he liked eating horses and carrots,');
