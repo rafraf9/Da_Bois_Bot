6f761180aaddbdeea434a138a72c99f69591792c
rafraf9 <31069991+rafraf9@users.noreply.github.com>
2020-11-22 20:09:18 +0200

    Update rabbit.js

--- commands/rabbit.js
+++ commands/rabbit.js
@@ -5,6 +5,6 @@ module.exports = {
     {
         message.channel.send('once upon a time there was a rabbit, the rabbits name was fluffy and he liked eating horses and carrots,');
         message.channel.send('one time he was looking for a little tasty horse but he didnt find!!!');
-        message.channel.send('so he didnt know what to do so finally fluffy decided to be a vegetarien and since then he ate just carrots.');
+        message.channel.send('so he didnt know what to do so finally fluffy decided to be a vegetarian and since then he ate just carrots.');
     }
 }
